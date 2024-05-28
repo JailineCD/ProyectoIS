75b6ac834edfe4a0f7258d6f7d5bec357c1970c7
Douglas Pérez <dperezq2@miumg.edu.gt>
2024-05-28 15:36:02 -0600

    Se agregó TipoExamenDTO

---
+++ lab1Final/src/main/java/umg/edu/gt/DTO/TipoExamenDTO.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package umg.edu.gt.DTO;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Douglas
+ */
+@Entity
+@Table(name = "signo_vital")
+public class TipoExamenDTO implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    
+    @Column(name = "clave")
+    private String clave;
+    
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the clave
+     */
+    public String getClave() {
+        return clave;
+    }
+
+    /**
+     * @param clave the clave to set
+     */
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+}
