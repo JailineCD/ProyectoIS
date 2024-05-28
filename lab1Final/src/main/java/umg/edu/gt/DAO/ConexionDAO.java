9635ae38d71497529a81073e725deb3242412407
lvelasquezpb <148801363+lvelasquezsUMG@users.noreply.github.com>
2024-05-28 09:12:34 -0600

    corrigiendo clase de conexion.

--- lab1Final/src/main/java/umg/edu/gt/DAO/ConexionDAO.java
+++ lab1Final/src/main/java/umg/edu/gt/DAO/ConexionDAO.java
@@ -7,81 +7,42 @@ import org.hibernate.cfg.Configuration;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.Properties;
 
 public class ConexionDAO {
     
-    private String url = "";
-    private String usuario = "";
-    private String contraseña = "";
+    private static SessionFactory sessionFactory;
 
-    public void Credenciales() {
-        Properties prop = new Properties();
+    static {
         try {
-            File configDirec = new File(System.getProperty("catalina.base"));
-            File configFile = new File(configDirec, "conf/umg-filesystem.properties");
-            InputStream stream;
-            stream = new FileInputStream(configFile);
-            prop.load(stream);
-
-            usuario = prop.getProperty("usuario");
-            contraseña = prop.getProperty("contraseña");
-            url = prop.getProperty("url");
-            System.out.println("Se ha obtenido las credenciales correctamente.");
-        } catch (Exception ex) {
-            System.out.println("Error al obtener las credenciales." + ex);
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
         }
     }
 
-    public Connection conexionMysql() throws Exception {
-        this.Credenciales();
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conexion = DriverManager.getConnection(getUrl(), getUsuario(), getContraseña());
-        System.out.println("la conexion es exitosa: " + conexion);
-        return conexion;
+    public Session obtenerSesion() {
+        return sessionFactory.openSession();
     }
 
-    /**
-     * @return the url
-     */
-    public String getUrl() {
-        return url;
+    public void CerrarSesion(Session session) {
+        if (session != null) {
+            session.close();
+        }
     }
 
-    /**
-     * @param url the url to set
-     */
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    /**
-     * @return the usuario
-     */
-    public String getUsuario() {
-        return usuario;
-    }
-
-    /**
-     * @param usuario the usuario to set
-     */
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    /**
-     * @return the contraseÃ±a
-     */
-    public String getContraseña() {
-        return contraseña;
-    }
-
-    /**
-     * @param contraseña the contraseña to set
-     */
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
+    public void Credenciales(){
+        Properties prop = new Properties();
+        try {
+            File configDirec = new File(System.getProperty("catalina.base"), "conf");
+            File configFile = new File(configDirec, "umg-filesystem.properties");
+            InputStream stream = new FileInputStream(configFile);
+            prop.load(stream);
+            String usuario = prop.getProperty("usuario");
+            String contraseña = prop.getProperty("clave");
+            String url = prop.getProperty("url");
+        } catch (Exception ex) {
+            System.out.println("Error de credenciales: " + ex);
+        }
     }
 }
