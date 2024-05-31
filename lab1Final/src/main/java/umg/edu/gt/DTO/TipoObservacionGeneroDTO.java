736318804ba8de157a57d0aceff1b12bb35fea61
lvelasquezpb <148801363+lvelasquezsUMG@users.noreply.github.com>
2024-05-31 10:35:23 -0600

    dto tipo observacion genero

---
+++ lab1Final/src/main/java/umg/edu/gt/DTO/TipoObservacionGeneroDTO.java
@@ -0,0 +1,140 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package umg.edu.gt.DTO;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Luis Velasquez
+ */
+@Entity
+@Table(name = "tipo_observacion_genero")
+public class TipoObservacionGeneroDTO implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    
+    @Column(name = "clave")
+    private String clave;
+    
+    @Column(name = "descripcion")
+    private String descripcion;
+    
+    @Column(name = "create_time")
+    private LocalDateTime create_time;
+    
+    @Column(name = "update_time")
+    private LocalDateTime update_time;
+    
+    @Column(name = "estado")
+    private String estado = "Activo";  // Default value
+
+    @PrePersist
+    @PreUpdate
+    public void prePersistUpdate() {
+        setCreate_time(LocalDateTime.now());
+        setUpdate_time(LocalDateTime.now());
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the clave
+     */
+    public String getClave() {
+        return clave;
+    }
+
+    /**
+     * @param clave the clave to set
+     */
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the create_time
+     */
+    public LocalDateTime getCreate_time() {
+        return create_time;
+    }
+
+    /**
+     * @param create_time the create_time to set
+     */
+    public void setCreate_time(LocalDateTime create_time) {
+        if (this.create_time == null) {
+        this.create_time = create_time;
+    }
+    }
+
+    /**
+     * @return the update_time
+     */
+    public LocalDateTime getUpdate_time() {
+        return update_time;
+    }
+
+    /**
+     * @param update_time the update_time to set
+     */
+    public void setUpdate_time(LocalDateTime update_time) {
+        if (this.id != null) {
+            this.update_time = update_time;
+        }
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
